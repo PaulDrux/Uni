6206315d404023f627a0e98da776fc12c9287b8a
Paul Drux <paul.drux@t-online.de>
2020-04-06 17:45:01 +0200

    new methods

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,28 @@
 // Funktion zum Würfeln
-function wuerfeln() {
-  var zufallszahl = Math.floor(Math.random() * 6) + 1;
-  document.getElementById('currentnumberlabel').innerHTML = zufallszahl;
-  console.log(zufallszahl);
-  var counter = 0;
-  counter += zufallszahl;
-  console.log(counter);
-  // updateZwischenstand(zufallszahl);
+var zufallszahl;
+var score = 0;
+
+function generate() {
+  zufallszahl = Math.floor(Math.random() * 6) + 1;
+  return zufallszahl;
 }
 
-// function updateZwischenstand(zahl) {
-//   var ergebnis = [];
-//   ergebnis.push(zahl);
-//
-//   document.getElementById('zwischenstandlabel').innerHTML = count;
-// }
+function wuerfeln() {
+  console.log("Alt " + zufallszahl);
+  var x = generate();
+  document.getElementById('currentnumberlabel').innerHTML = zufallszahl;
+  console.log(x);
+  console.log("Neu " + zufallszahl);
+  changeIntermediateResult();
+}
+
+function changeIntermediateResult() {
+  score = score + zufallszahl;
+  console.log("Inter: " + score);
+  document.getElementById('zwischenstandlabel').innerHTML = score;
+}
+
+function reset() {
+  score = 0;
+  document.getElementById('zwischenstandlabel').innerHTML = score;
+}
